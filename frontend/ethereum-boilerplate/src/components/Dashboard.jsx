126508bc20d0a6265780e461c483a28eee6eb219
Hritwik Tripathi <tripathi.hritwik@gmail.com>
2022-01-19 20:38:24 +0530

    added uff

--- frontend/ethereum-boilerplate/src/components/Dashboard.jsx
+++ frontend/ethereum-boilerplate/src/components/Dashboard.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 export default function Dashboard() {
+
+  
+
   return <div>
       finally dashboard work krenge aahahhhjahjAJHASBJHSKSJNV
   </div>;
--- frontend/ethereum-boilerplate/src/components/Portfolio.jsx
+++ frontend/ethereum-boilerplate/src/components/Portfolio.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 export default function Portfolio() {
+
+
   return (
     <div>
-      <div>hahahahahaha joe mama</div>
+      <div>hahahahahaha joe mama's portfolio</div>
     </div>
   );
 }
